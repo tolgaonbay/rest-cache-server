8247f2e411e20fe2b837105e2a78e6d7083fa65f
tolgaonbay <tolgaonbay@yahoo.com>
2015-11-14 23:24:37 +0200

    refactoring - extracted service request to a function

--- rest-cache.js
+++ rest-cache.js
@@ -15,64 +15,68 @@ var server = http.createServer(function (request, response) {
     }
 
     request.pipe(concat(function (data) {
-        var postData = JSON.parse(data);
+        var serviceData = JSON.parse(data);
 
-        var postUrl = postData.hostname + ':' + postData.port + postData.path;
+        var postUrl = getUrl(serviceData.hostname, serviceData.port, serviceData.path);
 
         var service = serviceCache[postUrl];
 
         if (service) {
-            response.end(service.cache);
+            response.end(service.result);
             
             console.log('sent from cache');
         } else {
-            service = postData;
+            sendRequest(serviceData, function (service) {
+                serviceCache[postUrl] = service;
 
-            console.log('load cache: ' + service);
-
-            var jsonData = JSON.stringify(service.data);
-
-            var options = {
-              hostname: service.hostname,
-              port: service.port,
-              path: service.path,
-              method: service.method
-            };
-
-            if (jsonData) {
-                options.headers = {
-                    'Content-Type': 'application/json',
-                    'Content-Length': jsonData.length
-                }
-            }
-
-            var clientRequest = http.request(options, function(clientResponse) {
-                clientResponse.pipe(concat(function (data) {
-                    console.log(data.toString());
-
-                    if (service.isXml) {
-                        xml2js.parseString(data.toString(), function (err, result) {
-                            service.cache = JSON.stringify(result);
-                        });
-                    } else {
-                        service.cache = data.toString();
-                    }
-
-                    serviceCache[postUrl] = service;
-
-                    response.end(service.cache);
-                }));
+                response.end(service.result);
             });
-
-            if (jsonData)
-                clientRequest.write(jsonData);
-
-            clientRequest.end();
         }
     }));
 });
 
 server.listen(9999); 
+
+function sendRequest(service, processResult) {
+    console.log('load cache: ' + service);
+
+    var options = {
+      hostname: service.hostname,
+      port: service.port,
+      path: service.path,
+      method: service.method
+    };
+
+    var requestContent = JSON.stringify(service.data);
+
+    if (requestContent) {
+        options.headers = {
+            'Content-Type': 'application/json',
+            'Content-Length': requestContent.length
+        }
+    }
+
+    var clientRequest = http.request(options, function(clientResponse) {
+        clientResponse.pipe(concat(function (data) {
+            console.log(data.toString());
+
+            if (service.isXml) {
+                xml2js.parseString(data.toString(), function (err, result) {
+                    service.result = JSON.stringify(result);
+                });
+            } else {
+                service.result = data.toString();
+            }
+
+            processResult(service);
+        }));
+    });
+
+    if (requestContent)
+        clientRequest.write(requestContent);
+
+    clientRequest.end();
+}
 
 function getPath(urlInfo) {
     var index = urlInfo.path.indexOf('?');
@@ -82,4 +86,8 @@ function getPath(urlInfo) {
     }
 
     return urlInfo.path.substring(0, index);
+}
+
+function getUrl(hostname, port, path) {
+    return hostname + ':' + port + path;
 }

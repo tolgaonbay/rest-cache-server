d146ffd70031a05d2b32154d13123dcde4059116
tolgaonbay <tolgaonbay@yahoo.com>
2015-12-13 22:42:10 +0200

    added invalid request path check unit test

---
+++ test/test.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var RequestProcessor = require('../request-processor');
+
+describe('request-processor', function () {
+    describe('#process()', function () {
+       it('should return error if the request path is not valid', function () {
+            var requestProcessor = new RequestProcessor();
+
+            var request = {
+                url : "http://localhost:9000/api/"
+            };
+
+            var mockResponse = {
+                end: function() {}
+            };
+
+            sinon.stub(mockResponse, 'end');
+
+            requestProcessor.process(request, mockResponse);
+
+            assert.equal(mockResponse.statusCode, 404);
+
+            sinon.assert.calledOnce(mockResponse.end);
+            sinon.assert.calledWithMatch(mockResponse.end, '404 - Not Found');
+       });
+    });
+});
+
+//TODO: check if the result came from cache after first call
